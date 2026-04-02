8c6db776b4c023e57e3e26ab33aa371bf7f7f595
Helcio Anicio <helcioanicio19@gmail.com>
2026-04-01 22:56:54 -0300

    Feat: Redesign project

--- src/components/ui/cost.tsx
+++ src/components/ui/cost.tsx
@@ -3,12 +3,13 @@ import { IoMdCloseCircle } from "react-icons/io";
 import Image from "next/image";
 import webSiteRival from "../../../public/webSiteRival.webp";
 import webSiteMine from "../../../public/webSiteMine.webp";
+import { Card, CardContent } from "./card";
 
 export const Cost = () => {
   return (
     <section className="flex w-full justify-center">
-      <article className="flex w-full max-w-5xl flex-col gap-16 px-2">
-        <h2 className="text-3xl font-bold underline underline-offset-2 sm:text-4xl">
+      <article className="flex w-full max-w-5xl flex-col gap-16 px-2 py-20">
+        <h2 className="text-3xl font-bold sm:text-4xl">
           O custo invisível de um site medíocre
         </h2>
         <div className="flex flex-col gap-10 sm:gap-16">
@@ -39,33 +40,35 @@ export const Cost = () => {
               </div>
             </div>
           </div>
-          <div className="m-auto flex w-full flex-col items-center justify-center gap-y-7 px-10">
-            <div className="flex w-full items-start justify-center gap-3 text-sm md:gap-5">
-              <IoMdCloseCircle className="size-6 min-w-max text-destructive sm:size-10" />
-              <p className="text-center sm:w-1/2">
-                <span>Velocidade de Percepção (LCP):</span> Se demorar mais de
-                2.5s para o seu site abrir, o cliente desiste da compra.
-              </p>
-              <FaCheckCircle className="size-6 min-w-max text-green-500 sm:size-10" />
-            </div>
-            <div className="flex w-full items-start justify-center gap-3 text-sm md:gap-5">
-              <IoMdCloseCircle className="size-6 min-w-max text-destructive sm:size-10" />
-              <p className="text-center sm:w-1/2">
-                <span>Capacidade de Resposta (FID):</span> Mede o atraso entre
-                um clique e a ação. Sites travados não vendem.
-              </p>
-              <FaCheckCircle className="size-6 min-w-max text-green-500 sm:size-10" />
-            </div>
-            <div className="flex w-full items-start justify-center gap-3 text-sm md:gap-5">
-              <IoMdCloseCircle className="size-6 min-w-max text-destructive sm:size-10" />
-              <p className="text-center sm:w-1/2">
-                <span>Estabilidade visual (CLS):</span> Sabe quando você vai
-                clicar em um botão e ele se desloca (“pula”)? Nós eliminamos
-                esses saltos irritantes.
-              </p>
-              <FaCheckCircle className="size-6 min-w-max text-green-500 sm:size-10" />
-            </div>
-          </div>
+          <Card className="m-auto border-none bg-transparent py-10">
+            <CardContent className="flex flex-col items-center justify-center gap-y-7">
+              <div className="flex w-full items-start justify-center gap-3 text-sm md:gap-5">
+                <IoMdCloseCircle className="size-6 min-w-max text-destructive sm:size-10" />
+                <p className="text-center sm:w-1/2">
+                  <span>Velocidade de Percepção (LCP):</span> Se demorar mais de
+                  2.5s para o seu site abrir, o cliente desiste da compra.
+                </p>
+                <FaCheckCircle className="size-6 min-w-max text-green-500 sm:size-10" />
+              </div>
+              <div className="flex w-full items-start justify-center gap-3 text-sm md:gap-5">
+                <IoMdCloseCircle className="size-6 min-w-max text-destructive sm:size-10" />
+                <p className="text-center sm:w-1/2">
+                  <span>Capacidade de Resposta (FID):</span> Mede o atraso entre
+                  um clique e a ação. Sites travados não vendem.
+                </p>
+                <FaCheckCircle className="size-6 min-w-max text-green-500 sm:size-10" />
+              </div>
+              <div className="flex w-full items-start justify-center gap-3 text-sm md:gap-5">
+                <IoMdCloseCircle className="size-6 min-w-max text-destructive sm:size-10" />
+                <p className="text-center sm:w-1/2">
+                  <span>Estabilidade visual (CLS):</span> Sabe quando você vai
+                  clicar em um botão e ele se desloca (“pula”)? Nós eliminamos
+                  esses saltos irritantes.
+                </p>
+                <FaCheckCircle className="size-6 min-w-max text-green-500 sm:size-10" />
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </article>
     </section>
