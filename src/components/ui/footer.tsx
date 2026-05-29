3372bc53f5fc98737b9f6b1769b9c43eca6ea215
Helcio Anicio <helcioanicio19@gmail.com>
2026-05-29 00:56:28 -0300

    Fix: Removed import phone icon

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,38 +1,31 @@
 "use client";
 
-// import { Github, Globe, MessageCircle } from "lucide-react";
-import {  Phone } from "lucide-react";
 import Image from "next/image";
 import Logo3 from "../../../public/logo3.svg";
 import Logo2 from "../../../public/logo2.svg";
 import { useTheme } from "next-themes";
-import { ButtonGeral } from './buttonGeral';
-
-// const socials = [
-//   { href: "#", label: "WhatsApp", icon: MessageCircle },
-//   { href: "#", label: "GitHub", icon: Github },
-//   { href: "#", label: "Website", icon: Globe },
-// ];
+import { ButtonGeral } from "./buttonGeral";
 
 export const Footer = () => {
   const { theme } = useTheme();
   return (
     <footer className="m-auto w-full space-y-5 py-20" id="footer">
       <section id="contato" className="w-full px-6 lg:px-8">
-        <div className="mx-auto grid max-w-5xl gap-16 ">
+        <div className="mx-auto grid max-w-5xl gap-16">
           <div>
             <p className="mb-3 text-sm font-semibold uppercase tracking-[0.3em]">
               Contato
             </p>
-            <h2 className="text-3xl font-bold md:text-5xl w-3/4">
+            <h2 className="w-3/4 text-3xl font-bold md:text-5xl">
               Leve a sua presença no mercado para o nível profissional!
             </h2>
             <p className="mt-6 max-w-2xl text-xl leading-8 text-secondary-foreground/80">
-              Se você quer melhorar seu site atual ou começar um projeto do zero com máxima performance, entre em contato. Respondemos em até 24h.
+              Se você quer melhorar seu site atual ou começar um projeto do zero
+              com máxima performance, entre em contato. Respondemos em até 24h.
             </p>
 
             <div className="mt-10 flex gap-4">
-               <ButtonGeral
+              <ButtonGeral
                 text={"Quero um site que converte"}
                 link={
                   "https://wa.me/5531991973835?text=Olá+,+quero+um+site+que+converte!"
