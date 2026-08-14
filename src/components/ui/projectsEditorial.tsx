38a3b0a91d295cfa02d4ba8308d4bca1a7dc5437
Helcio <helcioanicio19@gmail.com>
2026-08-14 10:18:19 -0300

    Fix: Removed wrong import

--- src/components/ui/projectsEditorial.tsx
+++ src/components/ui/projectsEditorial.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { ArrowUpRight } from "lucide-react";
 import { projects } from "@/data/projects";
 import { ButtonGeral } from "./buttonGeral";
 
